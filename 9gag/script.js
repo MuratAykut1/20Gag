5b30e7e30e482d9195594b92baa73e1a126fffd2
MuratCiftci <44578841+MuratCiftci@users.noreply.github.com>
2020-03-29 17:04:12 +0300

    Add files via upload

---
+++ 9gag/script.js
@@ -0,0 +1,16 @@
+function dropdownMenu() {
+    document.getElementById("#dropdown-menu").classList.toggle("show");
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn') ) {
+      var dropdowns = document.getElementsByClassName("popup-menu");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
